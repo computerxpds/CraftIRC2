31f8a84ee81f11c796953affecd6501ed0468577
Dakota Strickner <dstrickner@gmail.com>
2013-07-02 01:01:11 -0400

    updated to 1.6

--- src/com/ensifera/animosity/craftirc/CraftIRC.java
+++ src/com/ensifera/animosity/craftirc/CraftIRC.java
@@ -22,7 +22,7 @@ import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.craftbukkit.v1_5_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_6_R1.CraftServer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.RegisteredServiceProvider;
